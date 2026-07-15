c514d9799632c7accd7ab72c2569e3cec0524acd
Felipe <53532151+felipevianaa7@users.noreply.github.com>
2026-07-15 11:55:03 -0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDRxzCw9FJ-gSFfznw0EAl7HBqnrCPM6GQ",
+
+  authDomain: "casamento-suelen-felipe.firebaseapp.com",
+
+  projectId: "casamento-suelen-felipe",
+
+  storageBucket: "casamento-suelen-felipe.firebasestorage.app",
+
+  messagingSenderId: "304619466326",
+
+  appId: "1:304619466326:web:6be173a3612945781e55f7"
+
+};
